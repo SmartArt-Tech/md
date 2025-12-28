644a0cbc8009b2a57397f78353769be85bf25487
sbr5136014 <sbr5136014@gmail.com>
2025-12-28 17:09:27 -0500

    Changes to be committed:  	deleted:    obj/Debug/net48/App.g.cs  	modified:   obj/Debug/net48/MarkdownViewer.AssemblyInfo.cs  	modified:   obj/Debug/net48/MarkdownViewer.AssemblyInfoInputs.cache  	deleted:    obj/Debug/net48/MarkdownViewer_MarkupCompile.cache

--- obj/Debug/net48/MarkdownViewer.AssemblyInfo.cs
+++ obj/Debug/net48/MarkdownViewer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarkdownViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0f28aaafa9e7a3306f897870dad0fdd9f9ecfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95dc827da71911414a0287656449cf43ab1034d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarkdownViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Markdown Viewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
